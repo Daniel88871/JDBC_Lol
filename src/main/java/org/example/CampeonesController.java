7c4fe6fa2e4d8da5219202aa8a6f04138fbc9abd
Daniel88871 <druizl01@elpuig.xeill.net>
2023-01-30 17:52:43 +0100

    Initial commit

---
+++ src/main/java/org/example/CampeonesController.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.Connection;
+
+public class CampeonesController {
+
+	private Connection connection;
+	
+	public CampeonesController(Connection c) {
+		this.connection = connection;
+	}
+
+
+
+}
---
+++ src/main/resources/LOL.sql
@@ -0,0 +1,21 @@
+CREATE TABLE campeones (
+nombre VARCHAR (400),
+popularidad VARCHAR (400),
+porcentaje_de_victoria VARCHAR (400),
+porcentaje_de_baneo VARCHAR (400),
+kda VARCHAR (400),
+pentas_por_partida VARCHAR (400)
+);
+
+
+CREATE TABLE hechizos (
+nombre VARCHAR (50),
+popularidad VARCHAR (50),
+porcentaje_de_victoria VARCHAR (50)
+);
+
+
+CREATE TABLE objetos (
+popularidad VARCHAR (100),
+porcentaje_de_victoria VARCHAR (100)
+);
